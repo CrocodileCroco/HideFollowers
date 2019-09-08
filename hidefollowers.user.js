34317c66c6d39360ebdbfd241e67c8c548f82490
LL Productions FR <CrocodileCroco@users.noreply.github.com>
2019-09-08 18:25:23 +0200

    added youtube (new) and instagram

--- hidefollowers.user.js
+++ hidefollowers.user.js
@@ -2,12 +2,28 @@
 // @name HideFollowers
 // @namespace Violentmonkey Scripts
 // @match https://twitter.com/*
+// @match https://www.youtube.com/*
+// @match https://www.instagram.com/*
 // @grant none
 // @require https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // ==/UserScript==
 
-// Old Twitter
+//!Twitter (Only Old version because of CORS problem)
+
 setInterval(function(){
   $('.ProfileCardStats').remove()
   $('a[data-nav="followers"]').remove()
+}, 1);
+
+//!Instagram
+
+setInterval(function(){
+  $('a[href$="/followers/"]').remove()
+}, 1);
+
+//!YouTube
+
+setInterval(function(){
+  $('yt-formatted-string[id="subscriber-count"]').remove()
+  $('yt-formatted-string[class="style-scope ytd-subscribe-button-renderer"]').remove()
 }, 1);
